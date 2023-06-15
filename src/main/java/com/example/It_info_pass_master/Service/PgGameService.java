d691fd85e054c60ebdba381ee1fde1ae06a8d3d9
kenta-saito <k.saito@tc-tech.co.jp>
2023-06-15 11:46:00 +0900

    初期実装確認済み

--- src/main/java/com/example/It_info_pass_master/Service/PgGameService.java
+++ src/main/java/com/example/It_info_pass_master/Service/PgGameService.java
@@ -2,7 +2,9 @@ package com.example.It_info_pass_master.Service;
 
 import com.example.It_info_pass_master.DAO.GameDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PgGameService implements GameService{
     @Autowired
     private GameDao gameDao;
