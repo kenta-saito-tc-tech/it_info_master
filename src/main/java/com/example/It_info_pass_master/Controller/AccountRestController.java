6cfb169f5223f91cd8f4fa9ad1934582c65359e1
kenta-saito <k.saito@tc-tech.co.jp>
2023-06-16 19:35:20 +0900

    メニュー実装済み（完璧）

--- src/main/java/com/example/It_info_pass_master/Controller/AccountRestController.java
+++ src/main/java/com/example/It_info_pass_master/Controller/AccountRestController.java
@@ -1,17 +1,16 @@
 package com.example.It_info_pass_master.Controller;
 
-import com.example.It_info_pass_master.Entity.IdPassRecord;
-import com.example.It_info_pass_master.Entity.UserRecord;
+import com.example.It_info_pass_master.Entity.*;
 import com.example.It_info_pass_master.Service.AccountService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 public class AccountRestController {
@@ -74,5 +73,58 @@ public class AccountRestController {
             return new ResponseEntity<>("POST request failed", HttpStatus.BAD_REQUEST);
         }
     }
+
+    /**
+     * ゲームスコアの上位３位の取得
+     *
+     * @param
+     * @return
+     */
+    @GetMapping("/ranking_select")
+    public List<RankingRecord> findTopThree(@RequestParam(name = "age") String age) {
+        try {
+            var list = accountService.findTopThree(age);
+            return list; //ステータスコード200番
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //ステータスコード400番
+        }
+    }
+
+    /**
+     * 年代の全取得
+     *
+     * @param
+     * @return
+     */
+    @GetMapping("/age_select")
+    public List<AgeRecord> findAgeAll() {
+        try {
+            var list = accountService.findAgeAll();
+            return list; //ステータスコード200番
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //ステータスコード400番
+        }
+    }
+
+    /**
+     * ユーザーのランキング取得
+     *
+     * @param
+     * @return
+     */
+    @GetMapping("/myRanking")
+    public MyRankRecord myRanking(@RequestParam(name = "age") String age, @RequestParam(name = "id") int id) {
+        try {
+            var myRank = accountService.findUserRank(age, id);
+            return myRank; //ステータスコード200番
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST); //ステータスコード400番
+        }
+    }
+
+
 
 }
--- src/main/java/com/example/It_info_pass_master/Service/AccountService.java
+++ src/main/java/com/example/It_info_pass_master/Service/AccountService.java
@@ -1,10 +1,15 @@
 package com.example.It_info_pass_master.Service;
 
-import com.example.It_info_pass_master.Entity.IdPassRecord;
-import com.example.It_info_pass_master.Entity.UserRecord;
+import com.example.It_info_pass_master.Entity.*;
+
+import java.util.List;
 
 public interface AccountService {
     public UserRecord findUser(IdPassRecord idPassRecord);
     public UserRecord checkIdExist(String id);
     public int insertUser(UserRecord userRecord);
+    public List<RankingRecord> findTopThree(String age);
+    public List<AgeRecord> findAgeAll();
+    public MyRankRecord findUserRank(String age, int id);
+
 }
