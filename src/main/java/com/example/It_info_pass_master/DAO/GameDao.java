f140eb7e9b3d0eb1e01545ed36d1eb950c617a87
seiya2032 <kkayou.seiya.2023.kensyu@gmail.com>
2023-06-17 22:09:49 +0900

    コミット　ゲームモード画面完成

--- src/main/java/com/example/It_info_pass_master/DAO/GameDao.java
+++ src/main/java/com/example/It_info_pass_master/DAO/GameDao.java
@@ -5,6 +5,6 @@ import com.example.It_info_pass_master.Entity.ageidRecord;
 import java.util.List;
 
 public interface GameDao {
-    public List<ageidRecord> userGameAdd(int userid, int ageid);
+    public ageidRecord userGameAdd(int userid, int ageid);
 
 }
--- src/main/java/com/example/It_info_pass_master/Service/GameService.java
+++ src/main/java/com/example/It_info_pass_master/Service/GameService.java
@@ -5,6 +5,6 @@ import com.example.It_info_pass_master.Entity.ageidRecord;
 import java.util.List;
 
 public interface GameService {
-    public List<ageidRecord> userGameAdd(int userid, int ageid);
+    public ageidRecord userGameAdd(int userid, int ageid);
 
 }
