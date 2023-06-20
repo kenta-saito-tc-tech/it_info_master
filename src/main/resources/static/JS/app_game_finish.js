c686cf88dd7ef2764992ae4adf0e18552da11ff4
taira kota <taira.kota.2023.kensyu@gmail.com>
2023-06-20 17:03:49 +0900

    Changes by axiz

--- src/main/resources/static/JS/app_game_finish.js
+++ src/main/resources/static/JS/app_game_finish.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     resultTime();
+    resultList();
 }
 
 function resultTime() {
@@ -21,3 +22,41 @@ function resultTime() {
         }
     });
 }
+
+function resultList() {
+    const userGameId = document.getElementById('userGameId').innerText;
+
+    fetch(`/game_finish/list?userGameId=${userGameId}`)
+    .then(res => {
+        if(res.status === 400) {
+          window.alert('Hello JavaScript');
+        } else {
+          res.json()
+          .then(data => {
+          const listElement = document.createElement('div');
+                data.forEach((list) => {
+                const divElement = document.createElement('div');
+                const marginElement = document.createElement('span');
+                const spanElement = document.createElement('span');
+                const buttonElement = document.createElement('button');
+                marginElement.textContent=list.questionName;
+                if(list.userAnswer == 1) {
+                    spanElement.textContent = "〇";
+                } else {
+                    spanElement.textContent ="✖";
+                }
+                buttonElement.type = "submit";
+                buttonElement.name = "info";
+                buttonElement.value = list.questionId;
+                buttonElement.textContent = "詳細";
+                divElement.appendChild(buttonElement);
+                divElement.appendChild(spanElement);
+                divElement.appendChild(marginElement);
+                listElement.appendChild(divElement);
+                })
+                const c = document.getElementById('list');
+                    c.appendChild(listElement);
+                })
+          }
+    })
+  }
