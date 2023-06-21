1aa9ca608bbdd7cb2e0d5082c6746478b037e867
taira kota <taira.kota.2023.kensyu@gmail.com>
2023-06-21 12:47:22 +0900

    問題作成画面　表示

---
+++ src/main/resources/static/JS/app_admin_question_make.js
@@ -0,0 +1,9 @@
+const testDown= document.getElementById("testDown");
+    testDown.innerHTML = `
+        <option class=testOption value="1">データベース</option>
+        <option class=testOption value="2">システム構成要素</option>
+        <option class=testOption value="3">情報処理</option>
+        <option class=testOption value="4">マネジメント</option>
+        <option class=testOption value="5">ネットワーク</option>
+        <option class=testOption value="6">セキュリティ</option>
+    `;
