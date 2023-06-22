86002cc2be7edc5ea2a681ee4f2768015aba03bb
taira kota <taira.kota.2023.kensyu@gmail.com>
2023-06-22 14:14:15 +0900

    終了画面　ゲームタイトル画面へ遷移 終了画面　メニュー画面へ遷移 終了画面　左上の数値を非表示

--- src/main/java/com/example/It_info_pass_master/Controller/ManageController.java
+++ src/main/java/com/example/It_info_pass_master/Controller/ManageController.java
@@ -3,9 +3,15 @@ package com.example.It_info_pass_master.Controller;
 import com.example.It_info_pass_master.Service.ManageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ManageController {
     @Autowired
     private ManageService manageService;
+
+    @GetMapping("/admin_question_set")
+    public String adminQuestionSet() {
+        return "admin_question_set";
+    }
 }
